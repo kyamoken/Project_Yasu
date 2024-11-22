660f440fde7e5d325dbd76c160cf350302b88306
k_kamo <kamoken0531@gmail.com>
2024-11-22 11:07:34 +0900

    admin.pyを編集

--- Online_15/admin.py
+++ Online_15/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CustomUser, Post, Comment, Like, CommentLike, Announcement
+
+admin.site.register(CustomUser)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(CommentLike)
+admin.site.register(Announcement)
